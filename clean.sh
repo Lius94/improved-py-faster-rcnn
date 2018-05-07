81481597543737dfc46e7192b15d09aac6292b5a
Lius94 <939010676@qq.com>
2018-05-07 20:59:49 +0800

    删除

--- clean.sh
+++
@@ -1,5 +0,0 @@
-#clean_cache.sh
-rm -f data/cache/voc_2007_trainval_gt_roidb.pkl
-rm -f data/VOCdevkit2007/annotations_cache/annots.pkl
-rm -f `find . -name '*.pyc'`
-rm -rf output/faster_rcnn_end2end
